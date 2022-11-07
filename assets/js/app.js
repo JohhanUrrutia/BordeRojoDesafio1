96f574db6223a78f74bf707382f57934aa2a697c
Johhan Urrutia <johhanurrutia@MacBook-Pro-de-Johhan.local>
2022-11-06 22:13:56 -0300

    Correccion Boton v1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,15 @@
-var button = document.getElementById("buttonClick")
-button.ishid
-var image = document.querySelector(".imageClick")
+var image = document.querySelector(".imageClick");
 let contadorClicks = 0
 
-button.addEventListener("click", () => {
+image.addEventListener("click", () => {
     contadorClicks += 1
     console.log (contadorClicks)
 
     if (contadorClicks == 1) {
         image.style.border = "2px solid red"
-        console.log("me diste un click")
     }else if (contadorClicks > 1){
         contadorClicks = 0
         image.style.border = ""
-        console.log("me diste dos o mas click")
     }
 }) 
 
